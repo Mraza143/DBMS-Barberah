eb14ddd8cd599a3aad34439a5d29e04d8e4e409e
Raza <mraza6601@gmail.com>
2022-07-18 22:36:17 +0500

    Database Connection error solved. Dynamically Sample Table of  created. Environment variables issues resolved

--- server/app.js
+++ server/app.js
@@ -2,45 +2,68 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+//const fileUpload = require('express-fileupload')
 var path = require('path');
 var cors = require('cors')
 const errorMiddleware = require('./middleware/error')
 const dotenv = require("dotenv")
-var multer = require('multer');
+
 
 
 // Config
-// dotenv.config({ path: '../.env' })
-// ../.env
+// require('dotenv').config({ path: '../.env' })
+dotenv.config({ path: '../.env' })
+
 
 // Builtin Middlewares
 const corsOptions = {
     origin: true,
+    // origin: "*",
     credentials: true,
     optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser());
+//app.use(fileUpload())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+    // app.use(express.urlencoded({ extended: false }))
+   
+//app.use(cors())
+//app.use(cors({
+/*app.use(cors({
+    //origin : "http://localhost:3000",
+    origin :true,
+    credentials:true,
+    optionsSuccessStatus:200
+  }));*/
+
+/*app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});*/
+//app.use(cors())
+/*app.use(cors({
+    preflightContinue: true,
+    origin : "http://localhost:8080",
+    credentials: true,
+  }));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });*/
+
+
 
 // Routes 
-// const user = require("./routes/userRoutes");
-// const barber = require("./routes/barberRoutes");
-// const salon = require("./routes/salonRoutes");
-// const appointment = require("./routes/appointmentRoutes")
-// const review = require("./routes/reviewRoutes")
-
-// app.use("/api", user);
-// app.use("/api/salons", salon);
-// app.use('/api/barbers', barber);
-// app.use('/api/appointments', appointment)
-// app.use('/api/reviews', review)
+const router = require('./routes/productRouter.js')
+app.use('/api/products', router)
 
 
-
-// Middleware for Errors
-// app.use(errorMiddleware)
 
 module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,32 +1,21 @@
+//const express = require('express')
+//const cors = require('cors')
+
 const app = require("./app");
-// const path = require("path")
+require('dotenv').config();
+
+// middleware
+// routers
 
 
-// Handling uncaught exception
-process.on('uncaughtException', (err) => {
-    console.log(`Error: ${err.message}`)
-    console.log('Shutting down the server due to uncaught exception')
-    process.exit(1)
-})
-
-require('dotenv').config({ path: '../.env' })
 
 
-// Connecting Database
-// connectDB()
+//port
 
-PORT = 5000
-    // Server Starts Listening to http://localhost:5000
-const server = app.listen(PORT, () =>
-    console.log(`Server started on port ${PORT}`)
-)
+const PORT = process.env.PORT;
 
+//server
 
-// Handling Unhandled Rejection
-process.on('unhandledRejection', (err) => {
-    console.log(`Error: ${err.message}`)
-    console.log('Shutting down the server due to unhandled Promise Rejection')
-    server.close(() => {
-        process.exit(1)
-    })
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
